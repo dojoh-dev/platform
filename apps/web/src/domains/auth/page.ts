4b47264ba7fc68721d1390681f2c66496937b6ee
Simón Villafañe <simonvillafanee@gmail.com>
2026-05-04 00:21:45 -0300

    feat(auth): login at 70%

--- apps/web/src/domains/auth/page.ts
+++ apps/web/src/domains/auth/page.ts
@@ -1,5 +1,7 @@
 import { effect } from '@repo/shared/stateful';
 
+import eyeClosedIcon from '../../assets/lucid-icons/eye-closed.svg?import';
+import eyeIcon from '../../assets/lucid-icons/eye.svg?import';
 import styles from './page.module.css';
 
 export const metadata = {
@@ -8,16 +10,82 @@ export const metadata = {
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const traverseNodes = (node: NodeListOf<ChildNode>): Array<ChildNode> => {
-	const childNodes: Array<ChildNode> = [];
-	node.forEach((child) => {
-		if (child.childNodes.length > 0) {
-			childNodes.push(...traverseNodes(child.childNodes));
-		} else {
-			childNodes.push(child);
-		}
-	});
-	return childNodes;
+const encryptText = async (
+	element: HTMLElement,
+	finalText: string,
+	opt: { tickDelay: number; wait: number } = {
+		tickDelay: 50,
+		wait: 500,
+	}
+) => {
+	const symbols = '1 0';
+
+	const getRandomSymbol = () =>
+		symbols[Math.floor(Math.random() * symbols.length)];
+
+	const originalText = element.textContent || '';
+	const maxLength = Math.max(originalText.length, finalText.length);
+
+	const paddedFinal = finalText.padEnd(maxLength, ' ');
+
+	// Current visible chars
+	const current = originalText.padEnd(maxLength, ' ').split('');
+
+	// 1. Randomly replace chars with symbols
+	const touchedIndices = new Set<number>();
+
+	const encryptionLoop = async (): Promise<void> =>
+		new Promise((resolve) => {
+			const tick = () => {
+				if (touchedIndices.size >= maxLength) {
+					resolve();
+					return;
+				}
+
+				let idx: number;
+
+				do {
+					idx = Math.floor(Math.random() * maxLength);
+				} while (touchedIndices.has(idx));
+
+				current[idx] = getRandomSymbol();
+				element.textContent = current.join('');
+				touchedIndices.add(idx);
+
+				setTimeout(tick, opt.tickDelay);
+			};
+
+			tick();
+		});
+
+	const decryptionLoop = async (): Promise<void> =>
+		new Promise((resolve) => {
+			const tick = () => {
+				if (touchedIndices.size === 0) {
+					element.textContent = finalText;
+					resolve();
+					return;
+				}
+
+				let idx: number;
+
+				do {
+					idx = Math.floor(Math.random() * maxLength);
+				} while (!touchedIndices.has(idx));
+
+				current[idx] = paddedFinal[idx];
+				element.textContent = current.join('');
+				touchedIndices.delete(idx);
+
+				setTimeout(tick, opt.tickDelay);
+			};
+
+			tick();
+		});
+
+	await encryptionLoop();
+	await wait(opt.wait); // Stupid grace time?
+	await decryptionLoop();
 };
 
 export default function component() {
@@ -96,11 +164,6 @@ export default function component() {
 			bubbleCursor.style.transform = `translate(${currentTranslate.x}px, ${currentTranslate.y}px) scale(${currentScale})`;
 		};
 
-		const setScaleOnly = (scale: number) => {
-			currentScale = scale;
-			updateTransform();
-		};
-
 		let isCursorInside = false;
 
 		leftSide.addEventListener('mouseleave', () => {
@@ -111,6 +174,8 @@ export default function component() {
 		leftSide.addEventListener('mouseenter', () => {
 			isCursorInside = true;
 			document.documentElement.style.cursor = 'none';
+			bubbleCursor.style.height = '200px';
+			bubbleCursor.style.width = '200px';
 		});
 
 		const FIXED_OFFSET = 20;
@@ -141,6 +206,72 @@ export default function component() {
 		});
 	});
 
+	// Password visibility toggle
+	effect(() => {
+		const toggleButton = document.querySelector<HTMLButtonElement>(
+			'#toggle-password-visibility'
+		);
+		const passwordInput =
+			document.querySelector<HTMLInputElement>('#password');
+
+		if (!toggleButton) throw new Error('Toggle button not found');
+		if (!passwordInput) throw new Error('Password input not found');
+
+		toggleButton.addEventListener('click', () => {
+			const isPasswordVisible = passwordInput.type === 'text';
+			passwordInput.type = isPasswordVisible ? 'password' : 'text';
+			toggleButton.querySelector('img')!.src = isPasswordVisible
+				? eyeIcon
+				: eyeClosedIcon;
+			toggleButton.setAttribute(
+				'aria-label',
+				isPasswordVisible ? 'Show password' : 'Hide password'
+			);
+		});
+	});
+
+	// Switch to sign-up page (encryption animation)
+	effect(() => {
+		const signUpLink =
+			document.querySelector<HTMLAnchorElement>('#redirect-link');
+		const title = document.querySelector<HTMLHeadingElement>('#title');
+		const subtitle =
+			document.querySelector<HTMLParagraphElement>('#subtitle');
+
+		if (!signUpLink) throw new Error('Sign-up link not found');
+
+		signUpLink.addEventListener('click', (e) => {
+			e.preventDefault();
+		});
+
+		const titles = ['Welcome back!', 'Join the battle'];
+		const subtitles = [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac diam sit amet quam vehicula elementum sed.',
+		];
+		const links = ['Join the battle', 'Back to login'];
+
+		let currentIndex = 0;
+
+		signUpLink.addEventListener('click', () => {
+			currentIndex = (currentIndex + 1) % titles.length;
+
+			encryptText(title!, titles[currentIndex], {
+				tickDelay: 30,
+				wait: 300,
+			});
+			// Subtitle has a longer text, so we give it more time to encrypt/decrypt for better effect
+			encryptText(subtitle!, subtitles[currentIndex], {
+				tickDelay: 8,
+				wait: 50,
+			});
+			encryptText(signUpLink, links[currentIndex], {
+				tickDelay: 30,
+				wait: 300,
+			});
+		});
+	});
+
 	return /*html*/ `
 	<section class="${styles.login}">
 	    <div id="left-side" class="${styles.leftSide}">
@@ -166,6 +297,68 @@ export default function component() {
 	    </div>
 
 	    <div class="${styles.rightSide}">
+			<h1 id="title">Welcome back!</h1>
+			<p id="subtitle">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.
+			</p>
+
+			<form id="login-form">
+				<div class="${styles.inputGroup}">
+					<label for="email">Email / Nickname</label>
+					<div>
+						<input type="text" id="email" name="email" placeholder="johndoe@gmail.com / johndoe" />
+					</div>
+				</div>
+
+				<div class="${styles.inputGroup}">
+					<label for="password">Password</label>
+					<div>
+						<input type="password" id="password" name="password" placeholder="· · · · · · · · · · · ·" />
+						<span
+							id="toggle-password-visibility"
+							data-append
+							type="button"
+							aria-label="Toggle password visibility"
+						>
+							<img src="${eyeIcon}" alt="Toggle password visibility" width="16" height="16" />
+						</span>
+					</div>
+				</div>
+
+				<a href="/forgot-password">Forgot password?</a>
+
+				<button type="submit">Login</button>
+			</form>
+
+			<span class="${styles.or}"></span>
+
+			<div class="${styles.stackY}">
+				<button class="${styles.oauthButton}" data-provider="google">
+					<div aria-label="Google logo" width="16" height="16" ></div>
+					Continue with Google
+				</button>
+
+				<button class="${styles.oauthButton}" data-provider="github">
+					<div aria-label="Github logo" width="16" height="16" ></div>
+					Continue with Github
+				</button>
+
+				<button class="${styles.oauthButton}" data-provider="discord">
+					<div aria-label="Discord logo" width="16" height="16" ></div>
+					Continue with Discord
+				</button>
+			</div>
+
+			<p class="${styles.signUpPrompt}">
+				Ready to prove your skills?
+				<a
+					id="redirect-link"
+					href="#redirect-up"
+					title="Create account"
+				>
+					Join the battle.
+				</a>
+			</p>
 	    </div>
 	</section>`;
 }
